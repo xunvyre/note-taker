70a4be43f8248f597f5b658beb461b17ef2cd697
xunvyre <victoria@thedavids.us>
2022-08-25 20:25:51 -0400

    html requests hooked up to server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const { notes } = require('./db/notes');
+const fs = require('fs');
+const path = require('path');
 
 //port
 const PORT = process.env.PORT || 3001;
@@ -9,20 +11,43 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 //functions to be moved later
 function createNewNote(body, notesArr)
 {
     const note = body;
     notesArr.push(note);
+    fs.writeFileSync
+    (
+        path.join(__dirname, './db/notes.json'),
+        JSON.stringify({ notes: notesArr }, null, 2)
+    );
     return note;
 };
 
+//api route(s)
 app.get('/api/notes', (req, res) =>
 {
     res.json(notes);
 });
 
+//GET routes
+app.get('/', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+//send any weird request back to main notes page, NOT index.html
+app.get('*', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//POST route(s)
 app.post('/api/notes', (req, res) =>
 {
     //use epoch ms to assign a unique id
